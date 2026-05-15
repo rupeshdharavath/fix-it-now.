eac18e7b98857436be6f061612dffca44b74be68
Umesh Dharavath <rupeshdharavath@gmail.com>
2026-05-15 09:54:56 +0530

    Added frontend env for vercel

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,9 @@
+
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+const baseURL =
+  import.meta.env.VITE_API_BASE_URL ||
+  "http://localhost:3000";
 
 export const api = axios.create({
   baseURL,
